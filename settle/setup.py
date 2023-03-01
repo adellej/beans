478b003b4d04dd993ba9c5f88fd00ac6ff1679ab
MartinCupak <30996725+MartinCupak@users.noreply.github.com>
2023-03-01 18:07:56 +0800

    all wring, package module mys be in subdir from where onec calls pip install

--- settle/setup.py
+++ settle/setup.py
@@ -3,7 +3,9 @@
 Setup for settle, the beans edition
 """
 import os
-from setuptools import setup, Extension
+import subprocess
+from setuptools import setup
+#from . import __version__
 
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
@@ -15,34 +17,41 @@ def read(fname):
     """Read a file"""
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+# The C/C++ coded shared library had it's own makefike
+# to build. Hence we call that rather than duplicating that
+# within setuptools
 
-def get_version():
-    """Get the version number of settle"""
-    from . import __version__
-    return __version_
+
+def build_install_libsettle():
+    """compile and install C/C++ library libsettle"""
+    result = subprocess.run("make install",
+                            shell=True,
+                            check=True,
+                            capture_output=True)
+    print(result.stdout, result.stderr)
+    if result.returncode == 0:
+        print("libsettle built OK.")
+    else:
+        print("libsettle bulid FAILED!")
+
+    return result.returncode
 
 
 # What is the reasonable version of numpy to start with?
 reqs = ['numpy>=1.16']
 
-extensions = [Extension('libsettle',
-                       ["src/library.c"],
-                       depends=["src/library.h"],
-                       include_dirs=["src"],
-              ),
-]
-
 setup(
-    name="settle",
-    #    packages=find_packages(exclude='test'),
+    name="pySettle",
+    packages=['settle'],
 
     description="Computes ignition conditions for Type I X-ray bursts using a multi-zone model of the Neutron star accreting layer",
     long_description=read('README.rst'),
-    licence='MIT',
+    license='MIT',
     author="Andrew Cumming",
     author_email='Andrew.Cumming@gmail.com',
     # include_package_data=True,
     keywords='settle',
+    # installresult=build_install_libsettle(),
 
     python_requires='!=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
     classifiers=[
