f09b1530c2c9c22f98ad202e1682c8c3fb545972
MartinCupak <30996725+MartinCupak@users.noreply.github.com>
2023-03-02 15:49:15 +0800

    modify the short functional test to work wih pySettle and adopted beans/settler.py

--- settle/test/try.py
+++ settle/test/try.py
@@ -2,20 +2,20 @@
 # settle calls are done via settler.py interface module.
 
 from __future__ import print_function
-import settler as se
+from pySettle import settler as se
 import numpy as np
 
 print("Settle Short Functional Test (SFT)...")
 
 try:
     # initialize settle interface
-    settl = se.settle()
+    settl = se.Settle()
 
     for i in range(10):
         print(settl.full(Z=0.02, X=0.5, M=0.1, F=0.1, C=0, R=11.2, Ma=1.4))
         print(settl.run(Z=0.02, X=0.5, M=0.1, R=11.2, Ma=1.4))
 
-    set2 = se.settle(F=0.5)
+    set2 = se.Settle(F=0.5)
     # having created the link with different default Flux will generate different output with the same compact call
     print(settl.run(Z=0.02, X=0.5, M=0.1, R=11.2, Ma=1.4), set2.run(Z=0.02, X=0.5, M=0.1, R=11.2, Ma=1.4))
     print()
