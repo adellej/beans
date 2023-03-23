1de12ee85c15cc8e39aebf0adcbecf81d6ea2225
MartinCupak <30996725+MartinCupak@users.noreply.github.com>
2023-03-23 11:40:40 +0800

    modif beansp SFT test to work from everywhere

--- tests/test_sft_beans.py
+++ tests/test_sft_beans.py
@@ -5,10 +5,26 @@
 # Beans.do_run() does not return anything,
 # hence we are testing for random run-time errors
 
+import os
+
 from beansp.beans import Beans
+
+local_data_dir = 'data'
+
+data_dir = os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), local_data_dir)
+
+if not os.path.isdir(data_dir):
+    os.makedirs(data_dir)
+
+# directory/filename_base
+# no file is actually created, but the directory must exist,
+# otherwise the test fails!
+written_data_filename_base = os.path.join(data_dir, 'dummy')
 
 B = Beans(nwalkers=20,
           nsteps=10,
-          run_id="../beansp/data/1808/dummy",
-          gti_checking=0)
+          run_id=written_data_filename_base,
+          gti_checking=0,
+          restart=True)
 B.do_run(analyse=False)
