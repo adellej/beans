bf796ed582e810852d94e705f49e1dbcec800409
MartinCupak <30996725+MartinCupak@users.noreply.github.com>
2023-04-03 19:13:09 +0800

    re-factored tests for module and re-shuffed data dirs

--- tests/test_runmodel.py
+++ tests/test_runmodel.py
@@ -17,15 +17,15 @@ def test_run_model():
     theta = 0.5, 0.015, 0.2, 2.1, 3.5, 0.108, 0.90, 0.5, 1.4, 11.2
 
     path_to_data_file_obs = (
-            pathlib.Path(__file__).resolve().parent.parent / "data" / "1808_obs.txt"
+            pathlib.Path(__file__).resolve().parent.parent / "beansp/data" / "1808_obs.txt"
         )
 
     path_to_data_file_bursts = (
-            pathlib.Path(__file__).resolve().parent.parent / "data" / "1808_bursts.txt"
+            pathlib.Path(__file__).resolve().parent.parent / "beansp/data" / "1808_bursts.txt"
         )
 
     path_to_data_file_gti = (
-            pathlib.Path(__file__).resolve().parent.parent / "data" / "1808_gti.txt"
+            pathlib.Path(__file__).resolve().parent.parent / "beansp/data" / "1808_gti.txt"
         )
 
 
