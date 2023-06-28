73d181600f1e0521f7d96fe79b76bdfbb6f54625
Duncan Galloway <Duncan.Galloway@monash.edu>
2023-06-28 13:22:28 +0200

    fixed test_runmodel.py

--- tests/test_runmodel.py
+++ tests/test_runmodel.py
@@ -15,7 +15,10 @@ def test_run_model():
     numburstssim = 3 # this needs to be an integer value of half the number of bursts you want to simulate. I.e. simulate this many from the reference burst in either direction. Don't forget to account for missed bursts!
     numburstsobs = 4  # number of observed bursts in your dataset
 
-    theta = 0.5, 0.015, 0.2, 2.1, 3.5, 0.108, 0.90, 0.5, 1.4, 11.2
+    # Ordering of parameters in tuple is (from v2.0.0 onwards):
+    # X, Z, Q_b, dist, xi_b, xi_p, mass, radius, f_a, f_E
+    # theta = 0.5, 0.015, 0.2, 2.1, 3.5, 0.108, 0.90, 0.5, 1.4, 11.2
+    theta = 0.5, 0.02, 0.36, 4.0, 0.56, 1.1, 1.4, 11.8, 1.0, 1.0
 
     path_to_data_file_obs = (
             pathlib.Path(__file__).resolve().parent.parent / "beansp/data" / "1808_obs.txt"
@@ -48,18 +51,13 @@ def test_run_model():
     # full model which is now also output
     # test, valid, full_model = runmodel(theta,y,tref,bstart,pflux, pfluxe, tobs,numburstssim,len(bstart),ref_ind,gti_checking, 1, st, et)
     test, valid, full_model = runmodel(theta,B)
-
-    #exp = [  0.37596756  , 2.57382977 ,  3.6951732  ,  5.22042564 ,  5.49339669, 5.76917402  , 7.5398804  ,129.9739876,  139.81100404 ,155.46956035]
-
     
-    # updated here with the new (corrected) alpha calculation, for
-    # v0.1.3 and later
-    exp = [0.36955303, 2.56867184, 3.51029901, 5.24244931, 5.39297104, 5.67404108, 6.33264429, 
-        # 130.38129617, 140.42569944, 155.456677] 
-        86.19418202, 92.83446824, 102.77134457]
-    # agreement with the previous run (from v<=1.5.0) isn't as good now,
-    # but still not bad, not sure why the (very small) discrepancy
-    result = np.allclose(test, exp, rtol=1e-4)
+    # updated here with the new ("physical" parameters), v2.0.0 and later
+    exp = [7.68277452e-02, 2.71329835, 3.86210558, # burst times (d)
+           5.67417971, 5.86224830, 6.32795291, 7.25595091, # fluences
+           95.2263036, 101.222238, 108.805858] # alphas
+
+    result = np.allclose(test, exp)#, rtol=1e-4)
 
     assert result
 
