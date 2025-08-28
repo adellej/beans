08245c364db9e74dec134ad3a2bce4e39bf886f9
Duncan Galloway <Duncan.Galloway@monash.edu>
2025-08-28 08:15:57 +0200

    bumped version number

--- beansp/__init__.py
+++ beansp/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Adelle Goodwin and Duncan Galloway"""
 __email__ = 'adelle.goodwin@curtin.edu.au'
-__version__ = '2.55.3dev'
+__version__ = '2.56.0'
 
 # this allows "from beans import *"
 # __all__ = ["beans"]
